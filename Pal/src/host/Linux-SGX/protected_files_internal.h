8cae47f3c7bef9ba2cd77fb997ead5da5407f781
Rafał Wojdyła <omeg@invisiblethingslab.com>
2020-02-12 14:10:20 +0100

    Make private functions static

--- Pal/src/host/Linux-SGX/protected_files_internal.h
+++ Pal/src/host/Linux-SGX/protected_files_internal.h
@@ -209,55 +209,54 @@ struct pf_context {
     char* debug_buffer; //!< Buffer for debug output
 };
 
-// private
-bool ipf_cleanup_filename(const char* src, char* dest);
-bool ipf_init_fields(pf_context_t pf);
-bool ipf_file_recovery(pf_context_t pf, const char* filename);
-bool ipf_init_existing_file(pf_context_t pf, const char* filename);
-bool ipf_init_new_file(pf_context_t pf, const char* clean_filename);
+static bool ipf_init_fields(pf_context_t pf);
+static bool ipf_file_recovery(pf_context_t pf, const char* filename);
+static bool ipf_init_existing_file(pf_context_t pf, const char* filename);
+static bool ipf_init_new_file(pf_context_t pf, const char* clean_filename);
 
-bool ipf_read_node(pf_context_t pf, pf_handle_t handle, uint64_t node_number, void* buffer,
-                   uint32_t node_size);
-bool ipf_write_node(pf_context_t pf, pf_handle_t handle, uint64_t node_number, void* buffer,
-                    uint32_t node_size);
+static bool ipf_read_node(pf_context_t pf, pf_handle_t handle, uint64_t node_number, void* buffer,
+                          uint32_t node_size);
+static bool ipf_write_node(pf_context_t pf, pf_handle_t handle, uint64_t node_number, void* buffer,
+                           uint32_t node_size);
 
-bool ipf_generate_secure_blob(pf_context_t pf, pf_key_t* key, const char* label,
-                              uint64_t physical_node_number, pf_mac_t* output);
-bool ipf_generate_secure_blob_from_user_kdk(pf_context_t pf, bool restore);
-bool ipf_init_session_master_key(pf_context_t pf);
-bool ipf_derive_random_node_key(pf_context_t pf, uint64_t physical_node_number);
-bool ipf_generate_random_meta_data_key(pf_context_t pf);
-bool ipf_restore_current_meta_data_key(pf_context_t pf);
+static bool ipf_generate_secure_blob(pf_context_t pf, pf_key_t* key, const char* label,
+                                     uint64_t physical_node_number, pf_mac_t* output);
+static bool ipf_generate_secure_blob_from_user_kdk(pf_context_t pf, bool restore);
+static bool ipf_init_session_master_key(pf_context_t pf);
+static bool ipf_derive_random_node_key(pf_context_t pf, uint64_t physical_node_number);
+static bool ipf_generate_random_meta_data_key(pf_context_t pf);
+static bool ipf_restore_current_meta_data_key(pf_context_t pf);
 
-file_node_t* ipf_get_data_node(pf_context_t pf);
-file_node_t* ipf_read_data_node(pf_context_t pf);
-file_node_t* ipf_append_data_node(pf_context_t pf);
-file_node_t* ipf_get_mht_node(pf_context_t pf);
-file_node_t* ipf_read_mht_node(pf_context_t pf, uint64_t mht_node_number);
-file_node_t* ipf_append_mht_node(pf_context_t pf, uint64_t mht_node_number);
-bool ipf_write_recovery_file(pf_context_t pf);
-bool ipf_set_update_flag(pf_context_t pf, bool flush_to_disk);
-void ipf_clear_update_flag(pf_context_t pf);
-bool ipf_update_all_data_and_mht_nodes(pf_context_t pf);
-bool ipf_update_meta_data_node(pf_context_t pf);
-bool ipf_write_all_changes_to_disk(pf_context_t pf, bool flush_to_disk);
-bool ipf_erase_recovery_file(pf_context_t pf);
-bool ipf_internal_flush(pf_context_t pf, bool flush_to_disk);
-bool ipf_do_file_recovery(pf_context_t pf, const char* filename, uint32_t node_size);
-bool ipf_pre_close(pf_context_t pf);
-bool ipf_clear_cache(pf_context_t pf);
+static file_node_t* ipf_get_data_node(pf_context_t pf);
+static file_node_t* ipf_read_data_node(pf_context_t pf);
+static file_node_t* ipf_append_data_node(pf_context_t pf);
+static file_node_t* ipf_get_mht_node(pf_context_t pf);
+static file_node_t* ipf_read_mht_node(pf_context_t pf, uint64_t mht_node_number);
+static file_node_t* ipf_append_mht_node(pf_context_t pf, uint64_t mht_node_number);
 
-pf_context_t ipf_open(const char* filename, pf_file_mode_t mode, bool create, pf_handle_t file,
-                      size_t real_size, const pf_key_t* kdk_key, bool enable_recovery);
-bool ipf_close(pf_context_t pf);
-size_t ipf_read(pf_context_t pf, void* ptr, size_t size);
-size_t ipf_write(pf_context_t pf, const void* ptr, size_t size);
-int64_t ipf_tell(pf_context_t pf);
-bool ipf_seek(pf_context_t pf, int64_t new_offset, int origin);
-bool ipf_get_eof(pf_context_t pf);
-pf_status_t ipf_get_error(pf_context_t pf);
-void ipf_clear_error(pf_context_t pf);
-bool ipf_flush(pf_context_t pf);
-bool ipf_remove(const char* filename);
+static bool ipf_write_recovery_file(pf_context_t pf);
+static bool ipf_set_update_flag(pf_context_t pf, bool flush_to_disk);
+static void ipf_clear_update_flag(pf_context_t pf);
+static bool ipf_update_all_data_and_mht_nodes(pf_context_t pf);
+static bool ipf_update_meta_data_node(pf_context_t pf);
+static bool ipf_write_all_changes_to_disk(pf_context_t pf, bool flush_to_disk);
+static bool ipf_erase_recovery_file(pf_context_t pf);
+static bool ipf_internal_flush(pf_context_t pf, bool flush_to_disk);
+static bool ipf_do_file_recovery(pf_context_t pf, const char* filename, uint32_t node_size);
+static bool ipf_pre_close(pf_context_t pf);
+//static bool ipf_clear_cache(pf_context_t pf);
+
+static pf_context_t ipf_open(const char* filename, pf_file_mode_t mode, bool create,
+                             pf_handle_t file, size_t real_size, const pf_key_t* kdk_key,
+                             bool enable_recovery);
+static bool ipf_close(pf_context_t pf);
+static size_t ipf_read(pf_context_t pf, void* ptr, size_t size);
+static size_t ipf_write(pf_context_t pf, const void* ptr, size_t size);
+//static int64_t ipf_tell(pf_context_t pf);
+static bool ipf_seek(pf_context_t pf, int64_t new_offset, int origin);
+//static bool ipf_get_eof(pf_context_t pf);
+//static pf_status_t ipf_get_error(pf_context_t pf);
+static void ipf_clear_error(pf_context_t pf);
+//static bool ipf_flush(pf_context_t pf);
 
 #endif
